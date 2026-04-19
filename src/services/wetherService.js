7c0aac7d45394686dd672a7ff999a9693beee977
vnodkumar <nvinodkumar384@gmail.com>
2026-04-19 23:15:52 +0530

    getWeather func inside weatherServices.js fetches data of city

---
+++ src/services/wetherService.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export async function getWeather(city='btm layout'){
+    const API_KEY=import.meta.env.VITE_API_KEY;
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    const res= await axios.get(url);
+    return res.data.main
+}
